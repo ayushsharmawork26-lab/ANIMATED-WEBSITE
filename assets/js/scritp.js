bf8d29532954337993e57a81a5ba31d42595b71f
AYUSH SHARMA <ayushsharma.work26@gmail.com>
2026-07-14 22:42:09 +0530

    Build portfolio UI with hero, projects, skills, and video animations

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -1,2 +1,19 @@
-const video1 = document.getElementById('projectVideo1');
-const video2 = document.getElementById('projectVideo2')
+const video1 = document.getElementById("projectVideo1");
+const video2 = document.getElementById("projectVideo2");
+const video3 = document.getElementById("projectVideo3");
+const hoversign = document.querySelector(".hover-sign");
+
+const videoList = [video1, video2, video3];
+
+videoList.forEach((video) => {
+    video.addEventListener("mouseenter", () => {
+        video.play();
+        hoversign.classList.add("active");
+    });
+
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+        video.currentTime = 0; // Optional: video ko start se reset karega
+        hoversign.classList.remove("active");
+    });
+});
