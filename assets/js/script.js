2ce2971eaf9d8bfffa34476625d80f32904e9555
AYUSH SHARMA <ayushsharma.work26@gmail.com>
2026-07-21 00:18:50 +0530

    resolve responsive issues and update mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const video1 = document.getElementById("projectVideo1");
 const video2 = document.getElementById("projectVideo2");
 const video3 = document.getElementById("projectVideo3");
 const hoversign = document.querySelector(".hover-sign");
+const menuIcon = document.querySelector(".menu-icon");
+const sidebar = document.querySelector(".sidebar");
+const closeIcon = document.querySelector(".close-icon");
 
 const videoList = [video1, video2, video3];
 
@@ -16,4 +19,12 @@ videoList.forEach((video) => {
         video.currentTime = 0; // Optional: video ko start se reset karega
         hoversign.classList.remove("active");
     });
+});
+
+menuIcon.addEventListener("click", () => {
+    sidebar.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+    sidebar.classList.remove("active");
 });
